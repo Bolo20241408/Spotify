2d9804abf4c2653b5fc2a1569203a295c229762e
Jean Emir Sanchez Reyes <jsanchezr2304@alumno.ipn.mx>
2025-05-08 19:17:38 -0600

    Update Spotify.cpp

--- Spotify.cpp
+++ Spotify.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+using namespace std;
+//Clases para crear canciones y nodo
+////////////////////////////////////
+
+/*
+forma de los nodos de la lista de canciones de spotify
+|------|---------------------|----------------|---------|-------|
+|      |                     |                |         |       |
+| ant  |  Nombre de cancion  |  Nom  artista  | Me gusta|  sig  |
+|______|_____________________|________________|_________|_______|
+
+Cada nodo esta creado con el sistema de 
+|_|Funcionalidades a implementar un menu o interfaz de registro 
+(crear min 5 usuarios con correo y contrasena)
+|_|Crear funcion de busqueda de una cancion
+|_|Funcionalidades a implementar un menu o interfaz de registro 
+|_|Funcionalidades a implementar un menu o interfaz de registro 
+|_|Funcionalidades a implementar un menu o interfaz de registro 
+*/
